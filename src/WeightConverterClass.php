573ec13465142a1ea5ca97444b7612e91aee4c29
Prince <infor@rockblings.com>
2023-01-16 14:43:26 +0100

    initial pest test for weight class

--- src/WeightConverterClass.php
+++ src/WeightConverterClass.php
@@ -4,4 +4,19 @@ namespace Jobadssist\WeightConverter;
 
 class WeightConverterClass
 {
+  protected float $kilograms;
+  public static function kilograms(float $kilograms): self
+  {
+    return new static($kilograms);
+  }
+
+  public function __construct(float $kilograms)
+  {
+    $this->kilograms = $kilograms;
+  }
+
+  public function toPounds(): float
+  {
+    return $this->kilograms * 2.20462;
+  }
 }
